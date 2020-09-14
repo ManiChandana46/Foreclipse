15593cd5c7feefc4d73b213d46f2be84dc52cd06
ManiChandana46 <manichandana46@gmail.com>
2020-09-14 12:25:01 +0530

    "some new message"

--- LtiSession2/src/com/model/HelloWorld.java
+++ LtiSession2/src/com/model/HelloWorld.java
@@ -3,7 +3,7 @@ package com.model;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello duniya");
         System.out.println("Think better live better");
         System.out.println("From cloned person");
 	}
